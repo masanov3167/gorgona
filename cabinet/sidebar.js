a7b587c7be0aa21ff8a1080380fbba8979e94501
masanov3167 <masanov3167@gmail.com>
2022-09-01 17:15:03 +0500

    vihod ishlatildi

--- cabinet/sidebar.js
+++ cabinet/sidebar.js
@@ -1,12 +1,43 @@
 const sidebar = document.querySelector('.sidebar');
 const nav = document.querySelector('.toggle');
 const item = document.querySelectorAll('.sidebar__item');
+const logOut = document.querySelector('.log__out');
+const modal = document.querySelector('.modal');
+const modalBody = document.querySelector('.modal__body');
+const logOutBtn = document.querySelector('.log__out__btn');
+const closeModal = document.querySelector('.close__modal__btn');
 
 let display = true
 toggleFn()
+
+window.addEventListener('resize', () =>{
+    if(window.screen.width <600){
+        display = false
+        toggleFn()
+    }
+
+    if(window.screen.width >601){
+        display = true
+        toggleFn()
+    }
+});
+
 nav.addEventListener('click', () =>{
     display = !display;
     toggleFn()
+})
+
+logOut.addEventListener('click', () =>{
+    modal.classList.add('show');
+})
+
+closeModal.addEventListener('click', () =>{
+    modal.classList.remove('show');
+})
+logOutBtn.addEventListener('click', () =>{
+    window.localStorage.removeItem('token');
+    modal.classList.remove('show');
+    window.location.href = '/'
 })
 
 function toggleFn(){
