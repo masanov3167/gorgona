bd54ec69f66eb6ecccde2d6faf07d18e57e03bda
masanov3167 <masanov3167@gmail.com>
2022-09-01 18:53:47 +0500

    depozit page end

--- cabinet/main.js
+++ cabinet/main.js
@@ -1,7 +1,8 @@
 function copytext() {
-    var copyText = document.querySelector(".cabinet__item__body");
-  
-    navigator.clipboard.writeText(copyText.innerHTML.split('<button onclick="copytext()"')[0]);
-  
-    alert("Copied the text: " + copyText.innerHTML.split('<button onclick="copytext()"')[0]);
-  }
+    var range = document.createRange();
+    range.selectNode(document.querySelector(".cabinet__item__body"));
+    window.getSelection().removeAllRanges(); // clear current selection
+    window.getSelection().addRange(range); // to select text
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();// to deselect
+}
