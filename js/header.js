ee3badb5a3578c4037a67a760c24b36701af01bf
masanov3167 <masanov3167@gmail.com>
2022-11-11 01:07:32 +0500

    manimcha bo'ldiyov

--- js/header.js
+++ js/header.js
@@ -1,18 +1,7 @@
 const toggle = document.querySelector('.toggle');
-const close = document.querySelector('.close__nav')
 const navbar = document.querySelector('.header__nav')
-const link = document.querySelectorAll('.header__nav__link');
-
-for(let i of link){
-    i.addEventListener('click', () =>{
-        navbar.classList.remove('show__header__nav')
-    })
-}
 
 toggle.addEventListener('click', () =>{
-    navbar.classList.add('show__header__nav')
-})
-
-close.addEventListener('click', () =>{
-    navbar.classList.remove('show__header__nav')
+    navbar.classList.toggle('show__header__nav');
+    toggle.classList.toggle('active')
 })
