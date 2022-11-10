dfcf8289cfacb62846cd5f9a346e8c0cb406e524
masanov3167 <masanov3167@gmail.com>
2022-11-11 01:27:29 +0500

    frfr

--- js/header.js
+++ js/header.js
@@ -1,6 +1,13 @@
 const toggle = document.querySelector('.toggle');
 const navbar = document.querySelector('.header__nav')
 
+document.body.addEventListener('click', e =>{
+    if(!e.target.matches('.toggle') && !e.target.matches('.toggle__item')){
+        navbar.classList.remove('show__header__nav')
+        toggle.classList.remove('active')
+    }
+})
+
 toggle.addEventListener('click', () =>{
     navbar.classList.toggle('show__header__nav');
     toggle.classList.toggle('active')
